f02a429b35bfad880c5d585df23421f8d52706b2
Léonce Bizi <108945533+leonce-bizindavyi@users.noreply.github.com>
2023-09-04 17:58:02 +0200

    Update index.js

--- src/pages/api/upload/index.js
+++ src/pages/api/upload/index.js
@@ -13,7 +13,7 @@ export const config = {
 export default async function uploadHandler(req, res) {
   if (req.method === 'POST') {
     const form = new formidable.IncomingForm({
-      maxFileSize: 30 * 1024 * 1024, // 50 Go
+      maxFileSize: 30 * 1024 * 1024, // 30 Mo
     });
     form.multiples = true;
     // Parsez la requête
